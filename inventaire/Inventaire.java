b70701b356538a56f8486b8849fa9ff43dd6e310
ElTogo <felix.doyon@hotmail.com>
2022-05-04 16:36:29 -0400

    Inventaire
    Add a singleton pattern in Inventaire class to make sure there's never more than one inventory

--- inventaire/Inventaire.java
+++ inventaire/Inventaire.java
@@ -5,11 +5,22 @@ import ingredients.Ingredient;
 import java.util.ArrayList;
 
 public class Inventaire {
+    private static Inventaire instanceInventaire;
+
     private ArrayList<Ingredient> lesIngredients = new ArrayList<Ingredient>();
+
+    static Inventaire getInstance()
+    {
+        if (instanceInventaire == null)
+        {
+            instanceInventaire = new Inventaire();
+        }
+        return instanceInventaire;
+    }
 
     public void ajouter (Ingredient ingredient)
     {
         lesIngredients.add(ingredient);
     }
-
+    public void retirer (Ingredient ingredient) { lesIngredients.remove(ingredient); }
 }
