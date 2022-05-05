847f68baf9424d4be42962165d5d593246349e33
ElTogo <felix.doyon@hotmail.com>
2022-05-05 13:30:39 -0400

    enumType typePlat

--- menufact/plats/FactoryPlat.java
+++ menufact/plats/FactoryPlat.java
@@ -1,24 +1,21 @@
 package menufact.plats;
 
 public class FactoryPlat {
-    static PlatAuMenu getPlat(String type, int code, String description, double prix, double arg1, double arg2, double arg3)
+    public PlatAuMenu getPlat(TypePlat type, int code, String description, double prix, double arg1, double arg2, double arg3)
     {
-        if (type == "PlatAuMenu")
+        if (type == TypePlat.PLATAUMENU)
         {
             return new PlatAuMenu(code, description, prix);
         }
-        else if (type == "PlatSante")
+        else if (type == TypePlat.PLATSANTE)
         {
             return new PlatSante(code, description, prix, arg1, arg2, arg3);
         }
-        else if (type == "PlatEnfant")
+        else if (type == TypePlat.PLATENFANT)
         {
             return new PlatEnfant(code, description, prix, arg1);
         }
-        else
-        {
-            throw new MenuException("Type de plat invalide");
-        }
+        return null;
 
     }
 }
