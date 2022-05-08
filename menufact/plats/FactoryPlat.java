720bf5175d73076aec2a37a9caae8f8ea80f4ef1
ElTogo <felix.doyon@hotmail.com>
2022-05-08 10:44:14 -0400

    Update FactoryPlat.java

--- menufact/plats/FactoryPlat.java
+++ menufact/plats/FactoryPlat.java
@@ -3,19 +3,18 @@ package menufact.plats;
 public class FactoryPlat {
     public PlatAuMenu getPlat(TypePlat type, int code, String description, double prix, double arg1, double arg2, double arg3)
     {
-        if (type == TypePlat.PLATAUMENU)
-        {
-            return new PlatAuMenu(code, description, prix);
-        }
-        else if (type == TypePlat.PLATSANTE)
+        if (type == TypePlat.PLATSANTE)
         {
             return new PlatSante(code, description, prix, arg1, arg2, arg3);
         }
-        else if (type == TypePlat.PLATENFANT)
-        {
-            return new PlatEnfant(code, description, prix, arg1);
-        }
         return null;
+    }
 
+    public PlatAuMenu getPlat(TypePlat type, int code, String description, double prix)
+    {
+        if (type == TypePlat.PLATAUMENU)
+        {
+            return new PlatAuMenu(code, description, )
+        }
     }
 }
