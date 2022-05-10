623916b5d95299f272dd58daf6c1b7208e8dcd1d
Alex Lajeunesse <alexinlife.exe@gmail.com>
2022-05-10 15:41:51 -0400

    add JavaDoc

--- menufact/exceptions/MenuException.java
+++ menufact/exceptions/MenuException.java
@@ -1,7 +1,13 @@
 package menufact.exceptions;
 
+/**
+ * Erreur liée à la classe Menu
+ */
 public class MenuException extends Exception{
-
+    /**
+     * Constructeur de MenuException
+     * @param message Le message d'erreur à afficher
+     */
     public MenuException(String message){
         super("MenuException: " + message);
     }
--- menufact/plats/etats/EtatPlat.java
+++ menufact/plats/etats/EtatPlat.java
@@ -2,7 +2,21 @@ package menufact.plats.etats;
 
 import menufact.plats.PlatChoisi;
 
+/**
+ * MdC utilisé : State
+ * Interface d'EtatPlat
+ * Permet l'implémentation de nouveaux états pour PlatChoisi
+ */
 public interface EtatPlat {
+    /**
+     * Configure l'état du PlatChoisi passé en paramètre pour l'objet de la classe concrète
+     * @param plat Le plat devant changer d'état
+     */
     public void setEtatPlat(PlatChoisi plat);
+
+    /**
+     * Donne l'état provenant de l'enum assigné à la classe concrète
+     * @return L'état provenant de l'enum
+     */
     public EtatsPlat getEtatPlat();
 }
